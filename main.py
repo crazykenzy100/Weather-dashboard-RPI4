a90f8df87cfa126b02124c59b7b10f71a5335a43
kenzy <kenz092007@outlook.com>
2026-07-08 21:47:32 +0100

    added time and date

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from datetime import datetime
 
 #set up the main window 
 root = tk.Tk()
@@ -24,14 +25,36 @@ frame_sys.grid(row=0, column=1, sticky="nsew", padx=5, pady=5)
 frame_weather.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
 frame_calendar.grid(row=1, column=1, sticky="nsew", padx=5, pady=5)
 
+#dynamic functions with clock
+def update_clock():
+    #fetch the current time and date
+    current_time = datetime.now().strftime("%H:%M:%S")
+    current_date = datetime.now().strftime("%A, %d %B %Y")
+
+    #update the label text with fstring for seperate lines
+    label_time.config(text=f"{current_time}\n{current_date}")
+
+
+    root.after(1000, update_clock)  
+
 #------------------------------------ Testing/Production Code ------------------------------------#
 
 #placeholder label for time frame
-label_time = tk.Label(frame_time, text="Time and Date", fg="white", bg="black", font=("Helvetica", 24)).pack(expand=True)
-label_sys = tk.Label(frame_sys, text="System Info", fg="white", bg="blue", font=("Helvetica", 24)).pack(expand=True)
-label_weather = tk.Label(frame_weather, text="Weather Info", fg="white", bg="green", font=("Helvetica", 24)).pack(expand=True)
-label_calendar = tk.Label(frame_calendar, text="Calendar", fg="white", bg="yellow", font=("Helvetica", 24)).pack(expand=True)
+label_time = tk.Label(frame_time, text="Time and Date", fg="white", bg="black", font=("Helvetica", 24))
+label_time.pack(expand=True)
 
+label_sys = tk.Label(frame_sys, text="System Info", fg="white", bg="blue", font=("Helvetica", 24))
+label_sys.pack(expand=True)
+
+label_weather = tk.Label(frame_weather, text="Weather Info", fg="white", bg="green", font=("Helvetica", 24))
+label_weather.pack(expand=True)
+
+label_calendar = tk.Label(frame_calendar, text="Calendar", fg="white", bg="yellow", font=("Helvetica", 24))
+label_calendar.pack(expand=True)
+
+
+
+update_clock()  #initial call to start the clock update loop
 # keep it running
 root.mainloop()
 
